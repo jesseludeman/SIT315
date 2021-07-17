546c702b591655287ef4c7308c3eb25f582a5a25
Jesse Ludeman <jesse.ludeman@gmail.com>
2021-07-17 14:03:19 +1000

    Update main.cpp

--- Module1/main.cpp
+++ Module1/main.cpp
@@ -1,32 +1,30 @@
-// C++ code
-//
+// Jesse Ludeman - SIT315
 int pirSensor = 0;
 
 void setup()
 {
-      // PIR sensor
-      pinMode(3, INPUT);
-      // Red LED
-      pinMode(12, OUTPUT);
-      Serial.begin(9600);
+    // PIR sensor
+    pinMode(3, INPUT);
+    // Red LED
+    pinMode(12, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop()
 {
     pirSensor = digitalRead(3);
   
-      // Check if the PIR sensor reads a HIGH value.
-      // If true, turn the LED on.
-      if (pirSensor == HIGH)
+    // Check if the PIR sensor reads a HIGH value.
+    // If true, turn the LED on.
+    if (pirSensor == HIGH)
     {
          digitalWrite(12, HIGH);
-            // Print the output to the console
-          Serial.println("Movement detected");
+        Serial.println("Movement detected");
     }
-      else
+    else
     {
-         digitalWrite(12, LOW);
-          Serial.println("No movement detected");
+        digitalWrite(12, LOW);
+        Serial.println("No movement detected");
     }
   
       // Delay a bit to improve simulation

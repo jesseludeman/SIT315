a12649cc69a04019cfda32bc309ed53a2cbd57ca
Jesse Ludeman <jesse.ludeman@gmail.com>
2021-07-28 21:37:30 +1000

    Update Task1.2P.cpp

--- Module1/Task1.2P.cpp
+++ Module1/Task1.2P.cpp
@@ -1,12 +1,11 @@
-// Jesse Ludeman - SIT315
+// Jesse Ludeman - SIT315 - Task M1.T2P - Interrupt-driven board
 
 // PINs on the board.
 int pirSensor = 3;
-int BTN_PIN = 2;
+int push_button = 2;
 const int led_PIN = 12;
 
 // Global variables.
-//volatile uint8_t led_state = 0;
 volatile uint8_t led_state = 0;
 volatile uint8_t motion_state = LOW;
 
@@ -16,30 +15,30 @@ void setup()
   	pinMode(pirSensor, INPUT);
   	// Red LED.
   	pinMode(led_PIN, OUTPUT);
-    // Button PIN.
-    pinMode(BTN_PIN, INPUT_PULLUP);
-    // Attach the interrupt here.
-    attachInterrupt(digitalPinToInterrupt(pirSensor), pin_ISR, HIGH);
-  	attachInterrupt(digitalPinToInterrupt(BTN_PIN), alert, HIGH);
+    // Push button.
+    pinMode(push_button, INPUT_PULLUP);
+    // Attach the interrupts.
+    attachInterrupt(digitalPinToInterrupt(pirSensor), motion_alert, HIGH);
+  	attachInterrupt(digitalPinToInterrupt(push_button), led_alert, HIGH);
     Serial.begin(9600);
 }
 
 void loop()
 {
-    
+    // Nothing here!
 }
-void alert()
+void led_alert()
 {
-    // Logic for the interrupt goes here.
+    // Red LED interrupt.
     led_state = !led_state;
     digitalWrite(led_PIN, led_state);
-    Serial.println("Interrupt Occurred!");
+    Serial.println("Red LED Interrupt Detected!");
 }
 
-void pin_ISR() 
+void motion_alert()
 {
-    // Logic for the interrupt goes here.
+    // PIR sensor interrupt.
     motion_state = !motion_state;
     digitalWrite(led_PIN, motion_state);
-    Serial.println("Interrupt Occurred!");
+    Serial.println("Motion Detected!");
 }
